4b92714093712e786b9e2751e4f33f989c5762eb
Theophilus Ayebeng <theo.ayebeng@hotmail.com>
2026-08-02 09:01:37 +0000

    feat: implement service worker, PWA manifest, and static assets to enable offline functionality

--- dashboard/src/app/manifest.ts
+++ dashboard/src/app/manifest.ts
@@ -80,13 +80,13 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
     lang: 'en',
     icons: [
       {
-        src: '/icons/icon-192x192.png',
+        src: '/icons/icon-192x192-maskable.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/icons/icon-512x512.png',
+        src: '/icons/icon-512x512-maskable.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',

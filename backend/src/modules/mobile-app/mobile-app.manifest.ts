c1723fd91785594c275f40d1a5f48952517572fb
Theophilus Ayebeng <theo.ayebeng@hotmail.com>
2026-08-21 23:25:08 +0000

    chore: bump build version to 10

--- backend/src/modules/mobile-app/mobile-app.manifest.ts
+++ backend/src/modules/mobile-app/mobile-app.manifest.ts
@@ -1,7 +1,7 @@
 export const androidAppManifest = {
   platform: 'android',
   versionName: '1.0.1',
-  versionCode: 9,
+  versionCode: 10,
   apkUrl: 'https://tkclocking.online/apps/school-clocking-universal.apk',
   apkFileName: 'school-clocking-universal.apk',
   required: true,

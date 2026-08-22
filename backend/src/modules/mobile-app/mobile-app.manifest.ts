c1e2d2b3852e1134a82509a4627e337834b1354a
Theophilus Ayebeng <theo.ayebeng@hotmail.com>
2026-08-22 18:15:08 +0000

    chore: bump build version to 1.0.1+14 in pubspec.yaml

--- backend/src/modules/mobile-app/mobile-app.manifest.ts
+++ backend/src/modules/mobile-app/mobile-app.manifest.ts
@@ -1,7 +1,7 @@
 export const androidAppManifest = {
   platform: 'android',
   versionName: '1.0.1',
-  versionCode: 13,
+  versionCode: 14,
   apkUrl: 'https://tkclocking.online/apps/school-clocking-universal.apk',
   apkFileName: 'school-clocking-universal.apk',
   required: true,
